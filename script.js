ccd47157f29407d5f3d9abb3b6a59e987b44986a
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-03-01 00:33:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+var element = document.getElementById("level");
+var n = 0;
+
+while (element != null) {
+    n++;
+    element = element.parentNode;
+}
+
+alert("The level of the element is: " + n);
